3323a726634865306f870db42095140b949e6c1d
Jonny <21113168+Jonny0201@users.noreply.github.com>
2019-01-19 21:27:10 +0800

    Create data_structure.hpp

---
+++ data_structure/data_structure.hpp
@@ -0,0 +1,25 @@
+/*
+    * Copyright © [2019] [Jonny Charlotte]
+    *
+    * Licensed under the Apache License, Version 2.0 (the "License");
+    * you may not use this file except in compliance with the License.
+    * You may obtain a copy of the License at
+    *
+    *     http://www.apache.org/licenses/LICENSE-2.0
+    *
+    * Unless required by applicable law or agreed to in writing, software
+    * distributed under the License is distributed on an "AS IS" BASIS,
+    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    * See the License for the specific language governing permissions and
+    * limitations under the License.
+*/
+
+#ifndef DATA_STRUCTURE_DATA_STRUCTURE_HPP
+#define DATA_STRUCTURE_DATA_STRUCTURE_HPP
+
+#include "type_traits.hpp"
+#include "iterator.hpp"
+
+namespace ds = data_structure;
+
+#endif //DATA_STRUCTURE_DATA_STRUCTURE_HPP
