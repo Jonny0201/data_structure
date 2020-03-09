432f2ae90f948813d10e62fef94be73fe7abbd94
Jonny <21113168+Jonny0201@users.noreply.github.com>
2020-03-09 19:26:45 +0800

    Create heap.hpp

---
+++ data_structure/heap.hpp
@@ -0,0 +1,128 @@
+/*
+    * Copyright © [2019 - 2020] [Jonny Charlotte]
+    *
+    * Licensed under the Apache License, Version 2.0 (the "License");
+    * you may not use this file except in compliance with the License.
+    * You may obtain a copy of the License at
+    *
+    *     http://www.apache.org/licenses/LICENSE-2.0
+    *
+    * Unless required by applicable law or agreed to in writing, software
+    * distributed under the License is distributed on an "AS IS" BASIS,
+    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    * See the License for the specific language governing permissions and
+    * limitations under the License.
+*/
+
+#ifndef DATA_STRUCTURE_HEAP_HPP
+#define DATA_STRUCTURE_HEAP_HPP
+
+#include "type_traits.hpp"
+#include "iterator.hpp"
+#include "functional.hpp"
+
+namespace data_structure {
+    template <typename Iterator>
+    typename iterator_traits<Iterator>::difference_type distance(typename enable_if<
+            is_input_iterator<Iterator>::value and not is_random_access_iterator<Iterator>::value,
+            Iterator>::type begin, Iterator end);
+    template <typename Iterator>
+    typename iterator_traits<Iterator>::difference_type distance(
+            typename enable_if<is_random_access_iterator<Iterator>::value, Iterator>::type begin,
+            Iterator end);
+    ptrdiff_t distance(void *begin, void *end) noexcept;
+}
+
+namespace data_structure {
+    template <typename RandomAccessIterator>
+    inline void make_heap(RandomAccessIterator begin, RandomAccessIterator end)
+            noexcept(is_nothrow_swappable<typename iterator_traits<RandomAccessIterator>::value_type>::value){
+        ds::make_heap(begin, end, ds::less());
+    }
+    template <typename RandomAccessIterator, typename Compare>
+    void make_heap(RandomAccessIterator begin, RandomAccessIterator end, Compare cmp) {
+        const auto distance {ds::distance(begin, end)};
+        for(auto parent {(distance - 2) / 2}; parent >= 0; --parent) {
+            auto child {parent * 2 + 1};
+            if(const auto right_child {child + 1};
+                    right_child < distance and cmp(begin[child], begin[right_child])) {
+                ++child;
+            }
+            if(cmp(begin[child], begin[parent])) {
+                continue;
+            }
+            auto value {move(begin[parent])};
+            do {
+                begin[parent] = move(begin[child]);
+                parent = child;
+                if(++(child *= 2); child >= distance) {
+                    break;
+                }
+                if(const auto right_child {child + 1};
+                        right_child < distance and cmp(begin[child], begin[right_child])) {
+                    ++child;
+                }
+            }while(cmp(value, begin[child]));
+            begin[parent] = move(value);
+        }
+    }
+    template <typename RandomAccessIterator>
+    inline void push_heap(RandomAccessIterator begin, RandomAccessIterator end) {
+        push_heap(begin, end, ds::less());
+    }
+    template <typename RandomAccessIterator, typename Compare>
+    void push_heap(RandomAccessIterator begin, RandomAccessIterator end, Compare cmp) {
+        const auto distance {ds::distance(begin, end)};
+        if(distance < 2) {
+            return;
+        }
+        auto child {distance - 1}, parent {(child - 1) / 2};
+        if(cmp(begin[child], begin[parent])) {
+            return;
+        }
+        auto value {move(begin[child])};
+        do {
+            begin[child] = move(begin[parent]);
+            if(not parent) {
+                break;
+            }
+            child = parent;
+            --parent /= 2;
+        }while(cmp(begin[parent], value));
+        begin[parent] = move(value);
+    }
+    template <typename RandomAccessIterator>
+    inline void pop_heap(RandomAccessIterator begin, RandomAccessIterator end) {
+        ds::pop_heap(begin, end, ds::less());
+    }
+    template <typename RandomAccessIterator, typename Compare>
+    void pop_heap(RandomAccessIterator begin, RandomAccessIterator end, Compare cmp) {
+        const auto distance {ds::distance(begin, end)};
+        switch(distance) {
+            case 0:
+                [[fallthrough]];
+            case 1:
+                return;
+            default:
+                break;
+        }
+        --distance;
+        auto value {move(*--end)};
+        typename iterator_traits<RandomAccessIterator>::difference_type parent {0}, child {1};
+        if(const auto right_child {child + 1};
+                right_child < distance and cmp(begin[child], begin[right_child])) {
+            ++child;
+        }
+        while(true) {
+            begin[parent] = move(begin[child]);
+            parent = child;
+            ++(child *= 2);
+            if(child >= distance) {
+                break;
+            }
+        }
+        begin[parent] = move(value);
+    }
+}
+
+#endif //DATA_STRUCTURE_HEAP_HPP
