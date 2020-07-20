094c44e65b224bea7e801dc848fcfadb20cd09c1
Jonny <21113168+Jonny0201@users.noreply.github.com>
2020-07-20 13:35:55 +0800

    Update array.hpp

--- data_structure/array.hpp
+++ data_structure/array.hpp
@@ -17,7 +17,6 @@
 #ifndef DATA_STRUCTURE_ARRAY_HPP
 #define DATA_STRUCTURE_ARRAY_HPP
 
-#include "type_traits.hpp"
 #include "iterator.hpp"
 #include "algorithm.hpp"
 
@@ -40,9 +39,11 @@ namespace data_structure {
     public:
         value_type elements[Size];
     public:
+        [[nodiscard]]
         constexpr reference operator[](difference_type n) noexcept {
             return this->elements[n];
         }
+        [[nodiscard]]
         constexpr const_reference operator[](difference_type n) const noexcept {
             return this->elements[n];
         }
@@ -53,94 +54,125 @@ namespace data_structure {
         constexpr void swap(array &rhs) noexcept(is_nothrow_swappable<value_type>::value) {
             ds::swap_ranges(this->elements, this->elements + Size, rhs);
         }
+        [[nodiscard]]
         constexpr iterator begin() noexcept {
             return this->elements;
         }
+        [[nodiscard]]
         constexpr const_iterator begin() const noexcept {
             return this->elements;
         }
+        [[nodiscard]]
         constexpr iterator end() noexcept {
             return this->elements + Size;
         }
+        [[nodiscard]]
         constexpr const_iterator end() const noexcept {
             return this->elements + Size;
         }
+        [[nodiscard]]
         constexpr const_iterator cbegin() const noexcept {
             return this->elements;
         }
+        [[nodiscard]]
         constexpr const_iterator cend() const noexcept {
             return this->elements;
         }
+        [[nodiscard]]
         constexpr reverse_iterator rbegin() noexcept {
             return reverse_iterator(this->elements + (Size - 1));
         }
+        [[nodiscard]]
         constexpr const_reverse_iterator rbegin() const noexcept {
             return const_reverse_iterator(this->elements + (Size - 1));
         }
+        [[nodiscard]]
         constexpr reverse_iterator rend() noexcept {
             return reverse_iterator(this->elements - 1);
         }
+        [[nodiscard]]
         constexpr const_reverse_iterator rend() const noexcept {
             return const_reverse_iterator(this->elements - 1);
         }
+        [[nodiscard]]
         constexpr const_reverse_iterator rcbegin() const noexcept {
             return const_reverse_iterator(this->elements + (Size - 1));
         }
+        [[nodiscard]]
         constexpr const_reverse_iterator rcend() const noexcept {
             return const_reverse_iterator(this->elements - 1);
         }
+        [[nodiscard]]
         constexpr size_type size() const noexcept {
             return Size;
         }
+        [[nodiscard]]
         constexpr size_type max_size() const noexcept {
             return Size;
         }
+        [[nodiscard]]
         constexpr bool empty() const noexcept {
             return false;
         }
+        [[nodiscard]]
         constexpr reference at(difference_type n) noexcept {
             return (*this)[n];
         }
+        [[nodiscard]]
         constexpr const_reference at(difference_type n) const noexcept {
             return (*this)[n];
         }
+        [[nodiscard]]
         constexpr reference front() noexcept {
             return *this->elements;
         }
+        [[nodiscard]]
         constexpr const_reference front() const noexcept {
             return *this->elements;
         }
+        [[nodiscard]]
         constexpr reference back() noexcept {
             return *(this->elements + (Size - 1));
         }
+        [[nodiscard]]
         constexpr const_reference back() const noexcept {
             return *(this->elements + (Size - 1));
         }
+        [[nodiscard]]
         constexpr pointer data() noexcept {
             return this->elements;
         }
+        [[nodiscard]]
         constexpr const_pointer data() const noexcept {
             return this->elements;
         }
     };
-
-    template <typename T, decltype(sizeof 0) Size>
-    bool operator==(const array<T, Size> &lhs, const array<T, Size> &rhs)
+    
+    template <typename T, size_t Size>
+    void swap(const array<T, Size> &, const array<T, Size> &) noexcept;
+    template <typename T, size_t Size>
+    [[nodiscard]]
+    bool operator==(const array<T, Size> &, const array<T, Size> &)
             noexcept(has_nothrow_equal_to_operator<T>::value);
-    template <typename T, decltype(sizeof 0) Size>
-    bool operator!=(const array<T, Size> &lhs, const array<T, Size> &rhs)
+    template <typename T, size_t Size>
+    [[nodiscard]]
+    bool operator!=(const array<T, Size> &, const array<T, Size> &)
             noexcept(has_nothrow_equal_to_operator<T>::value);
-    template <typename T, decltype(sizeof 0) Size>
-    bool operator<(const array<T, Size> &lhs, const array<T, Size> &rhs)
+    template <typename T, size_t Size>
+    [[nodiscard]]
+    bool operator<(const array<T, Size> &, const array<T, Size> &)
             noexcept(has_nothrow_less_operator<T>::value);
-    template <typename T, decltype(sizeof 0) Size>
-    bool operator<=(const array<T, Size> &lhs, const array<T, Size> &rhs)
+    template <typename T, size_t Size>
+    [[nodiscard]]
+    bool operator<=(const array<T, Size> &, const array<T, Size> &)
             noexcept(has_nothrow_less_operator<T>::value);
-    template <typename T, decltype(sizeof 0) Size>
-    bool operator>(const array<T, Size> &lhs, const array<T, Size> &rhs)
+    template <typename T, size_t Size>
+    [[nodiscard]]
+    bool operator>(const array<T, Size> &, const array<T, Size> &)
             noexcept(has_nothrow_less_operator<T>::value);
-    template <typename T, decltype(sizeof 0) Size>
-    bool operator>=(const array<T, Size> &lhs, const array<T, Size> &rhs)
+    template <typename T, size_t Size>
+    [[nodiscard]]
+    bool operator>=(const array<T, Size> &, const array<T, Size> &)
             noexcept(has_nothrow_less_operator<T>::value);
 }
 
@@ -169,66 +201,82 @@ namespace data_structure {
     public:
         alignas(array_t) char_type elements[sizeof(array_t)];
     public:
-    public:
+        [[nodiscard]]
         constexpr void operator[](difference_type n) noexcept {
             static_assert(not __dsa::is_type<T>::value,
-                    "Cannot call array<T, 0>::operator[] on a zero-sized array!");
+                    "Cannot call array<NodeType, 0>::operator[] on a zero-sized array!");
         }
+        [[nodiscard]]
         constexpr void operator[](difference_type n) const noexcept {
             static_assert(not __dsa::is_type<T>::value,
-                    "Cannot call array<T, 0>::operator[] on a zero-sized array!");
+                    "Cannot call array<NodeType, 0>::operator[] on a zero-sized array!");
         }
     public:
         constexpr void fill(const_reference value) noexcept {
             static_assert(is_const<value_type>::value,
-                    "Cannot fill array<T, 0> held constant value_type!");
+                    "Cannot fill array<NodeType, 0> held constant value_type!");
         }
         constexpr void swap(array &rhs) noexcept(is_nothrow_swappable<value_type>::value) {
             static_assert(is_const<value_type>::value,
-                    "Cannot swap array<T, 0> held constant value_type!");
+                    "Cannot swap array<NodeType, 0> held constant value_type!");
         }
+        [[nodiscard]]
         constexpr iterator begin() noexcept {
             return this->elements;
         }
+        [[nodiscard]]
         constexpr const_iterator begin() const noexcept {
             return this->elements;
         }
+        [[nodiscard]]
         constexpr iterator end() noexcept {
             return this->elements;
         }
+        [[nodiscard]]
         constexpr const_iterator end() const noexcept {
             return this->elements;
         }
+        [[nodiscard]]
         constexpr const_iterator cbegin() const noexcept {
             return this->elements;
         }
+        [[nodiscard]]
         constexpr const_iterator cend() const noexcept {
             return this->elements;
         }
+        [[nodiscard]]
         constexpr reverse_iterator rbegin() noexcept {
             return reverse_iterator(this->elements);
         }
+        [[nodiscard]]
         constexpr const_reverse_iterator rbegin() const noexcept {
             return const_reverse_iterator(this->elements);
         }
+        [[nodiscard]]
         constexpr reverse_iterator rend() noexcept {
             return reverse_iterator(this->elements);
         }
+        [[nodiscard]]
         constexpr const_reverse_iterator rend() const noexcept {
             return const_reverse_iterator(this->elements);
         }
+        [[nodiscard]]
         constexpr const_reverse_iterator rcbegin() const noexcept {
             return const_reverse_iterator(this->elements);
         }
+        [[nodiscard]]
         constexpr const_reverse_iterator rcend() const noexcept {
             return const_reverse_iterator(this->elements);
         }
+        [[nodiscard]]
         constexpr size_type size() const noexcept {
             return 0;
         }
+        [[nodiscard]]
         constexpr size_type max_size() const noexcept {
             return 0;
         }
+        [[nodiscard]]
         constexpr bool empty() const noexcept {
             return true;
         }
@@ -254,9 +302,11 @@ namespace data_structure {
             static_assert(not __dsa::is_type<value_type>::value,
                     "Trying to get elements from a zero-sized array!");
         }
+        [[nodiscard]]
         constexpr pointer data() noexcept {
             return this->elements;
         }
+        [[nodiscard]]
         constexpr const_pointer data() const noexcept {
             return this->elements;
         }
@@ -264,7 +314,11 @@ namespace data_structure {
 }
 
 namespace data_structure {
-    template <typename T, decltype(sizeof 0) Size>
+    template <typename T, size_t Size>
+    inline void swap(const array<T, Size> &lhs, const array<T, Size> &rhs) noexcept {
+        lhs.swap(rhs);
+    }
+    template <typename T, size_t Size>
     bool operator==(const array<T, Size> &lhs, const array<T, Size> &rhs)
             noexcept(has_nothrow_equal_to_operator<T>::value) {
         for(auto i {0}; i < Size; ++i) {
@@ -274,12 +328,12 @@ namespace data_structure {
         }
         return true;
     }
-    template <typename T, decltype(sizeof 0) Size>
+    template <typename T, size_t Size>
     inline bool operator!=(const array<T, Size> &lhs, const array<T, Size> &rhs)
             noexcept(has_nothrow_equal_to_operator<T>::value) {
         return not(lhs == rhs);
     }
-    template <typename T, decltype(sizeof 0) Size>
+    template <typename T, size_t Size>
     bool operator<(const array<T, Size> &lhs, const array<T, Size> &rhs)
             noexcept(has_nothrow_less_operator<T>::value) {
         for(auto i {0}; i < Size; ++i) {
@@ -289,17 +343,17 @@ namespace data_structure {
         }
         return true;
     }
-    template <typename T, decltype(sizeof 0) Size>
+    template <typename T, size_t Size>
     bool operator<=(const array<T, Size> &lhs, const array<T, Size> &rhs)
             noexcept(has_nothrow_less_operator<T>::value) {
         return lhs < rhs or lhs == rhs;
     }
-    template <typename T, decltype(sizeof 0) Size>
+    template <typename T, size_t Size>
     bool operator>(const array<T, Size> &lhs, const array<T, Size> &rhs)
             noexcept(has_nothrow_less_operator<T>::value) {
         return rhs < lhs;
     }
-    template <typename T, decltype(sizeof 0) Size>
+    template <typename T, size_t Size>
     bool operator>=(const array<T, Size> &lhs, const array<T, Size> &rhs)
             noexcept(has_nothrow_less_operator<T>::value) {
         return not(lhs < rhs);
