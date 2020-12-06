492af336299257093d6929e84fe8d30a5b78bd2b
Jonny <21113168+Jonny0201@users.noreply.github.com>
2020-12-06 23:05:15 +0800

    Update algorithm.hpp

--- data_structure/algorithm.hpp
+++ data_structure/algorithm.hpp
@@ -73,6 +73,20 @@ namespace data_structure {
     inline void *advance(void *ptr, ptrdiff_t step = 1) noexcept {
         return reinterpret_cast<char *>(ptr) + step;
     }
+    template <typename Iterator>
+    inline Iterator next(Iterator it) noexcept(has_nothrow_pre_increment_operator_v<Iterator>) {
+        return ++it;
+    }
+    inline void *next(void *ptr) noexcept {
+        return reinterpret_cast<char *>(ptr) + 1;
+    }
+    template <typename Iterator>
+    inline Iterator previous(Iterator it) noexcept(has_nothrow_pre_decrement_operator_v<Iterator>) {
+        return --it;
+    }
+    inline void *previous(void *ptr) noexcept {
+        return reinterpret_cast<char *>(ptr) - 1;
+    }
 }
 __DATA_STRUCTURE_END
 
@@ -261,64 +275,248 @@ namespace data_structure {
 }
 __DATA_STRUCTURE_END
 
+__DATA_STRUCTURE_START(move)
+namespace data_structure {
+    template <typename InputIterator, typename OutputIterator>
+    OutputIterator move(InputIterator begin, InputIterator end, OutputIterator first) {
+        while(begin not_eq end) {
+            *first++ = ds::move(*begin++);
+        }
+        return first;
+    }
+    template <typename BidirectionalIterator, typename BidirectionalIteratorReturningType>
+    BidirectionalIteratorReturningType move_backward(BidirectionalIterator begin, BidirectionalIterator end,
+            BidirectionalIteratorReturningType last) {
+        while(begin not_eq end) {
+            *(--last) = ds::move(*(--end));
+        }
+        return last;
+    }
+}
+__DATA_STRUCTURE_END
+
+__DATA_STRUCTURE_START(rotate)
+namespace data_structure {
+    namespace __data_structure_auxiliary {
+        template <typename ForwardIterator>
+        inline ForwardIterator rotate_left(ForwardIterator begin, ForwardIterator end) {
+            iterator_traits_t(ForwardIterator, value_type) tmp {ds::move(*begin)};
+            auto result {ds::move(ds::next(begin), end, begin)};
+            *result = ds::move(tmp);
+            return result;
+        }
+        template <typename BidirectionalIterator>
+        inline BidirectionalIterator rotate_right(BidirectionalIterator begin, BidirectionalIterator end) {
+            auto before_end {ds::previous(end)};
+            iterator_traits_t(BidirectionalIterator, value_type) tmp {ds::move(*before_end)};
+            auto result {ds::move_backward(begin, before_end, end)};
+            *result = ds::move(*tmp);
+            return result;
+        }
+        template <typename ForwardIterator>
+        ForwardIterator rotate_forward(ForwardIterator begin, ForwardIterator middle, ForwardIterator end) {
+            for(auto i {middle};;) {
+                ds::swap(*begin++, *i++);
+                if(i == end) {
+                    break;
+                }
+                if(begin == middle) {
+                    middle = i;
+                }
+            }
+            auto result {begin};
+            if(begin not_eq middle) {
+                for(auto i {middle};;) {
+                    ds::swap(*begin++, *i++);
+                    if(i == end) {
+                        if(begin == middle) {
+                            break;
+                        }
+                        i = middle;
+                    }else if(begin == middle) {
+                        middle = i;
+                    }
+                }
+            }
+            return result;
+        }
+        template <typename Integral>
+        inline Integral gcd(Integral x, Integral y) {
+            do {
+                auto t {x % y};
+                x = y;
+                y = t;
+            }while(y);
+            return x;
+        }
+        template <typename RandomAccessIterator>
+        RandomAccessIterator rotate_gcd(RandomAccessIterator begin,
+                RandomAccessIterator middle, RandomAccessIterator end) {
+            const auto left {ds::distance(begin, middle)}, right {ds::distance(middle, end)};
+            if(left == right) {
+                ds::swap_ranges(begin, middle, end);
+                return middle;
+            }
+            auto gcd {ds::__dsa::gcd(left, right)};
+            for(auto i {begin + gcd}; i not_eq begin;) {
+                auto tmp {ds::move(*--i)};
+                auto p1 {i}, p2 {p1 + left};
+                do {
+                    *p1 = ds::move(*p2);
+                    p1 = p2;
+                    const auto distance {ds::distance(p2, end)};
+                    if (left < distance) {
+                        p2 += left;
+                    }else {
+                        p2 = begin + (left - distance);
+                    }
+                }while(p2 != i);
+                *p1 = ds::move(tmp);
+            }
+            return begin + right;
+        }
+        template <typename ForwardIterator>
+        inline ForwardIterator rotate_auxiliary(ForwardIterator begin, ForwardIterator middle,
+                enable_if_t<is_forward_iterator_v<ForwardIterator> and
+                        not is_bidirectional_iterator_v<ForwardIterator>, ForwardIterator> end) {
+            if constexpr(is_trivially_move_assignable_v<iterator_traits_t(ForwardIterator, value_type)>) {
+                if(ds::next(begin) == middle) {
+                    return ds::__dsa::rotate_left(begin, end);
+                }
+            }
+            return ds::__dsa::rotate_forward(begin, middle, end);
+        }
+        template <typename BidirectionalIterator>
+        inline BidirectionalIterator rotate_auxiliary(BidirectionalIterator begin, BidirectionalIterator middle,
+                enable_if_t<is_bidirectional_iterator_v<BidirectionalIterator> and
+                        not is_random_access_iterator_v<BidirectionalIterator>, BidirectionalIterator> end) {
+            if constexpr(is_trivially_move_assignable_v<iterator_traits_t(BidirectionalIterator, value_type)>) {
+                if(ds::next(begin) == middle) {
+                    return ds::__dsa::rotate_left(begin, end);
+                }
+                if (ds::next(middle) == end) {
+                    return ds::__dsa::rotate_right(begin, end);
+                }
+            }
+            return ds::__dsa::rotate_forward(begin, middle, end);
+        }
+        template <typename RandomAccessIterator>
+        inline RandomAccessIterator rotate_auxiliary(RandomAccessIterator begin, RandomAccessIterator middle,
+                enable_if_t<is_random_access_iterator_v<RandomAccessIterator>, RandomAccessIterator> end) {
+            if constexpr(is_trivially_move_assignable_v<iterator_traits_t(RandomAccessIterator, value_type)>)
+            {
+                if (ds::next(begin) == middle) {
+                    return ds::__dsa::rotate_left(begin, end);
+                }else if(ds::next(middle) == end) {
+                    return ds::__dsa::rotate_right(begin, end);
+                }
+                return ds::__dsa::rotate_gcd(begin, middle, end);
+            }
+            return ds::__dsa::rotate_forward(begin, middle, end);
+        }
+    }
+    template <typename ForwardIterator>
+    ForwardIterator rotate(ForwardIterator begin, ForwardIterator middle, ForwardIterator end) {
+        if(begin == middle) {
+            return end;
+        }else if(middle == end) {
+            return begin;
+        }
+        return ds::__dsa::rotate_auxiliary(begin, middle, end);
+    }
+}
+__DATA_STRUCTURE_END
+
 __DATA_STRUCTURE_START(sort)
 namespace data_structure {
     namespace __data_structure_auxiliary {
         struct quick_sort_selector {
             constexpr void operator()(dynamic, dynamic, dynamic) const noexcept {}
         };
+        template <typename Compare, typename Selector, typename RandomAccessIterator>
+        void quick_sort_auxiliary(RandomAccessIterator begin, RandomAccessIterator end,
+                iterator_traits_t(RandomAccessIterator, difference_type) left,
+                iterator_traits_t(RandomAccessIterator, difference_type) right,
+                Compare &compare, Selector &selector) {
+            if(left >= right) {
+                return;
+            }
+            auto left_cursor {left}, right_cursor {right};
+            selector(begin, end, compare);
+            auto pivot {ds::move(begin[right])};
+            auto move_flag {true};
+            while(true) {
+                if(move_flag) {
+                    while(compare(begin[left_cursor], pivot) and left_cursor < right_cursor) {
+                        ++left_cursor;
+                    }
+                    if(left_cursor < right_cursor) {
+                        begin[right_cursor--] = ds::move(begin[left_cursor]);
+                        move_flag = false;
+                        continue;
+                    }
+                }else {
+                    while(compare(pivot, begin[right_cursor]) and left_cursor < right_cursor) {
+                        --right_cursor;
+                    }
+                    if(left_cursor < right_cursor) {
+                        begin[left_cursor++] = ds::move(begin[right_cursor]);
+                        move_flag = true;
+                        continue;
+                    }
+                }
+                begin[left_cursor] = ds::move(pivot);
+                quick_sort_auxiliary(begin, end, left, left_cursor - 1, compare, selector);
+                quick_sort_auxiliary(begin, end, right_cursor + 1, right, compare, selector);
+                return;
+            }
+        }
     }
-    template <typename Compare, typename Selector, typename RandomAccessIterator>
-    void quick_sort(RandomAccessIterator, RandomAccessIterator,
-            iterator_traits_t(RandomAccessIterator, difference_type),
-            iterator_traits_t(RandomAccessIterator, difference_type),
-            Compare &, Selector &);
     template <typename Compare = less<>, typename Selector = __dsa::quick_sort_selector,
             typename RandomAccessIterator>
-    inline void quick_sort(RandomAccessIterator begin, RandomAccessIterator end, Compare compare = {},
-            Selector selector = {}) {
+    inline void quick_sort(RandomAccessIterator begin, RandomAccessIterator end,
+            Compare compare = {}, Selector selector = {}) {
         auto size {ds::distance(begin, end)};
         if(size <= 1) {
             return;
         }
-        ds::quick_sort(begin, end, 0, size - 1, compare, selector);
+        __dsa::quick_sort_auxiliary<add_lvalue_reference_t<Compare>, add_lvalue_reference_t<Selector>>(
+                begin, end, 0, size - 1, compare, selector);
     }
-    template <typename Compare, typename Selector, typename RandomAccessIterator>
-    void quick_sort(RandomAccessIterator begin, RandomAccessIterator end,
-            iterator_traits_t(RandomAccessIterator, difference_type) left,
-            iterator_traits_t(RandomAccessIterator, difference_type) right,
-            Compare &compare, Selector &selector) {
-        if(left >= right) {
-            return;
+
+    template <typename Compare = less<>, typename ForwardIterator>
+    void merge_sort(ForwardIterator begin, ForwardIterator end, Compare cmp = {}) {
+        const auto size {ds::distance(begin, end)};
+        switch(size) {
+            case 2:
+                if(auto next {ds::next(begin)}; not cmp(*begin, *next)) {
+                    ds::swap(*begin, *next);
+                }
+                [[fallthrough]];
+            case 0:
+                [[fallthrough]];
+            case 1:
+                return;
+            default:
+                break;
         }
-        auto left_cursor {left}, right_cursor {right};
-        selector(begin, end, compare);
-        auto pivot {ds::move(begin[right])};
-        auto move_flag {true};
-        while(true) {
-            if(move_flag) {
-                while(compare(begin[left_cursor], pivot) and left_cursor < right_cursor) {
-                    ++left_cursor;
+        auto mid {ds::advance(begin, size / 2)};
+        merge_sort<add_lvalue_reference_t<Compare>>(begin, mid, cmp);
+        merge_sort<add_lvalue_reference_t<Compare>>(mid, end, cmp);
+        /* O(1) space complexity */
+        for(auto i {begin}, j {mid}; j not_eq end;) {
+            do {
+                if(cmp(*j, *i)) {
+                    break;
                 }
-                if(left_cursor < right_cursor) {
-                    begin[right_cursor--] = ds::move(begin[left_cursor]);
-                    move_flag = false;
-                    continue;
+            }while(++i not_eq j);
+            auto middle {j};
+            do {
+                if(cmp(*i, *j)) {
+                    break;
                 }
-            }else {
-                while(compare(pivot, begin[right_cursor]) and left_cursor < right_cursor) {
-                    --right_cursor;
-                }
-                if(left_cursor < right_cursor) {
-                    begin[left_cursor++] = ds::move(begin[right_cursor]);
-                    move_flag = true;
-                    continue;
-                }
-            }
-            begin[left_cursor] = ds::move(pivot);
-            ds::quick_sort(begin, end, left, left_cursor - 1, compare, selector);
-            ds::quick_sort(begin, end, right_cursor + 1, right, compare, selector);
-            return;
+            }while(++j not_eq end);
+            ds::rotate(i, middle, j);
         }
     }
 }
