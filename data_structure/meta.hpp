5f15c50fc4a9c6850d055dde952986724ac554b0
Jonny <21113168+Jonny0201@users.noreply.github.com>
2021-04-21 10:18:18 +0800

    Copyright update

--- data_structure/meta.hpp
+++ data_structure/meta.hpp
@@ -1,5 +1,5 @@
 /*
-    * Copyright © [2019 - 2020] [Jonny Charlotte]
+    * Copyright © [2019 - 2021] [Jonny Charlotte]
     *
     * Licensed under the Apache License, Version 2.0 (the "License");
     * you may not use this file except in compliance with the License.
