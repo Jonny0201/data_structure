edf2c6d208d8c7ec351a0afa0f1f4fd6a04dbcc2
Jonny <21113168+Jonny0201@users.noreply.github.com>
2022-08-29 12:49:12 +0800

    Update memory.hpp

--- data_structure/memory.hpp
+++ data_structure/memory.hpp
@@ -29,7 +29,7 @@ namespace data_structure {
 }
 __DATA_STRUCTURE_END
 
-__DATA_STRUCTURE_START(data structure memory pool)
+__DATA_STRUCTURE_START(memory pool)
 namespace data_structure {
     template <size_t Align = 8, size_t MaxBytes = 128>
     class memory_pool {
@@ -210,10 +210,10 @@ namespace data_structure {
         if(nodes == 1) {
             return chunk;
         }
-        auto current_node {reinterpret_cast<free_list_node *>(chunk + size)};
+        auto current_node {static_cast<free_list_node *>(chunk + size)};
         *(free_list + free_list_index(size)) = current_node;
         while(--nodes > 1) {
-            auto next_node {reinterpret_cast<free_list_node *>(reinterpret_cast<char *>(current_node) + size)};
+            auto next_node {static_cast<free_list_node *>(static_cast<char *>(current_node) + size)};
             current_node->free_list_link = next_node;
             current_node = next_node;
         }
@@ -237,7 +237,7 @@ namespace data_structure {
         auto bytes_to_get {2 * total_bytes + round_up(chunk_size >> 4)};
         if(bytes_left > 0) {
             auto &first {*(free_list + free_list_index(bytes_left))};
-            auto new_first {reinterpret_cast<free_list_node *>(start)};
+            auto new_first {static_cast<free_list_node *>(start)};
             new_first->free_list_link = first;
             first = new_first;
         }
@@ -246,7 +246,7 @@ namespace data_structure {
             for(auto i {size}; i <= max_bytes; i += align) {
                 auto &first {*(free_list + free_list_index(i))};
                 if(first) {
-                    start = reinterpret_cast<char *>(first);
+                    start = static_cast<char *>(first);
                     first = first->free_list_link;
                     end = start + i;
                     return chunk_alloc(size, nodes);
