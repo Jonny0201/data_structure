d51d03c6eacc77c815f1e3e476da46c523b3fea8
Jonny <21113168+Jonny0201@users.noreply.github.com>
2023-11-26 14:56:44 +0800

    deprecate is_literal_type and is_pod

--- source/type_traits.hpp
+++ source/type_traits.hpp
@@ -963,7 +963,7 @@ template <typename T>
 constexpr inline auto is_complete_v {is_complete<T>::value};
 
 template <typename T>
-struct is_pod : bool_constant<__is_pod(T)> {};
+struct [[deprecated]] is_pod : bool_constant<__is_pod(T)> {};
 template <typename T>
 constexpr inline auto is_pod_v {is_pod<T>::value};
 
@@ -973,7 +973,7 @@ template <typename T>
 constexpr inline auto is_empty_v {is_empty<T>::value};
 
 template <typename T>
-struct is_literal_type : bool_constant<__is_literal_type(T)> {};
+struct [[deprecated]] is_literal_type : bool_constant<__is_literal_type(T)> {};
 template <typename T>
 constexpr inline auto is_literal_type_v {is_literal_type<T>::value};
 
