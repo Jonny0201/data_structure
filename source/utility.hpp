914f50fe4e39d1d4542408c1a5960de7d77dddb4
Jonny <21113168+Jonny0201@users.noreply.github.com>
2024-03-17 00:02:55 +0800

    add move and forward functions

---
+++ source/utility.hpp
@@ -0,0 +1,47 @@
+/*
+    * Copyright © [2019 - 2024] [Jonny]
+    *
+    * Licensed under the Apache License, Version 2.0 (the "License");
+    * you may not use this file except in compliance with the License.
+    * You may obtain a copy of the License at
+    *
+    *     http://www.apache.org/licenses/LICENSE-2.0
+    *
+    * Unless required by applicable law or agreed to in writing, software
+    * distributed under the License is distributed on an "AS IS" BASIS,
+    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    * See the License for the specific language governing permissions and
+    * limitations under the License.
+*/
+
+#ifndef DATA_STRUCTURE_UTILITY_HPP
+#define DATA_STRUCTURE_UTILITY_HPP
+
+#include "type_traits.hpp"
+
+namespace data_structure {
+
+__DATA_STRUCTURE_START(move and forward)
+template <typename T>
+inline constexpr remove_reference_t<T> &&move(T &&value) noexcept {
+    return static_cast<remove_reference_t<T> &&>(value);
+}
+template <bool Conditional, typename T>
+inline constexpr conditional_t<Conditional, remove_reference_t<T> &&, const T &>
+move_if(T &value) noexcept {
+    return move(value);
+}
+template <typename T>
+inline constexpr T &&forward(remove_reference_t<T> &value) noexcept {
+    return static_cast<T &&>(value);
+}
+template <typename T>
+inline constexpr T &&forward(remove_reference_t<T> &&value) noexcept {
+    static_assert(is_lvalue_reference_v<T>, "Cannot forward an rvalue as an lvalue!");
+    return static_cast<T &&>(value);
+}
+__DATA_STRUCTURE_END
+
+}       // namespace data_structure
+
+#endif //DATA_STRUCTURE_UTILITY_HPP
