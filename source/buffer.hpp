9ab910f0f41e827752974d916554f98fc2d65ceb
Jonny <21113168+Jonny0201@users.noreply.github.com>
2024-03-23 22:36:40 +0800

    add transaction for buffer and adjust the code style

--- source/buffer.hpp
+++ source/buffer.hpp
@@ -90,6 +90,8 @@ using buffer_compressed_pair = allocator_compressor<Allocator, is_empty_v<Alloca
 __DATA_STRUCTURE_END
 
 namespace data_structure {
+
+__DATA_STRUCTURE_START(buffer declaration)
 template <typename T, typename Allocator = allocator<T>>
 class buffer {
 public:
@@ -108,21 +110,28 @@ public:
     static_assert(is_same_v<T, typename Allocator::value_type>,
             "The value type of buffer should same as the allocator's value_type!");
 private:
+    struct exception_handler;
+    friend struct exception_handler;
+private:
     __dsa::buffer_compressed_pair<Allocator> buffer_size;
     pointer first;
 private:
+    template <bool CopyFromValue = true>
+    constexpr void initialize(const T &);
+    constexpr void move_to(pointer, size_type);
     template <typename InputIterator>
-    buffer(InputIterator, InputIterator, const Allocator &, false_type);
+    constexpr void initialize(InputIterator, InputIterator, false_type);
     template <typename ForwardIterator>
-    buffer(ForwardIterator, ForwardIterator, const Allocator &, true_type);
-private:
-    void move_to(pointer, size_type);
+    constexpr void initialize(ForwardIterator, ForwardIterator, true_type);
 public:
     buffer() = delete;
     explicit constexpr buffer(size_type, const Allocator & = {});
     constexpr buffer(size_type, const T &, const Allocator & = {});
-    template <IsInputIterator InputIterator>
+    template <size_type DefaultSize = 64, typename InputIterator> requires (is_input_iterator_v<InputIterator> and
+            not is_forward_iterator_v<InputIterator>)
     constexpr buffer(InputIterator, InputIterator, const Allocator & = {});
+    template <typename ForwardIterator> requires is_forward_iterator_v<ForwardIterator>
+    constexpr buffer(ForwardIterator, ForwardIterator, const Allocator & = {});
     constexpr buffer(initializer_list<T>, const Allocator & = {});
     buffer(const buffer &) = delete;
     buffer(buffer &&) = delete;
@@ -148,201 +157,137 @@ public:
     [[nodiscard]]
     constexpr Allocator allocator() const noexcept;
 };
-}
+__DATA_STRUCTURE_END
 
-namespace data_structure {
+__DATA_STRUCTURE_START(buffer implementation)
+/* private type */
+template <typename T, typename Allocator>
+struct buffer<T, Allocator>::exception_handler {
+    buffer &b;
+    size_type i;
+    explicit constexpr exception_handler(buffer &b, size_type i = 0) noexcept : b {b}, i {i} {}
+    constexpr void operator()() noexcept {
+        auto &allocator {this->b.buffer_size.allocator()};
+        ds::destroy(this->b.first, this->i);
+        allocator.deallocate(this->b.first, this->b.buffer_size());
+    }
+};
+
 /* private functions */
 template <typename T, typename Allocator>
-void buffer<T, Allocator>::move_to(pointer new_buffer, size_type old_size) {
+constexpr void buffer<T, Allocator>::move_to(pointer new_buffer, size_type old_size) {
     auto &allocator {this->buffer_size.allocator()};
     if constexpr(is_trivially_copy_assignable_v<T>) {
         ds::memory_copy(new_buffer, this->first, sizeof(T) * old_size);
     }else {
-        constexpr auto nothrow_move_constructible {is_nothrow_move_constructible_v<T>};
-        if constexpr(nothrow_move_constructible or is_nothrow_copy_constructible_v<T>) {
-            for(auto i {0}; i < old_size; ++i) {
-                ds::construct(new_buffer + i, ds::move_if<nothrow_move_constructible>(this->first[i]));
-            }
-        }else {
-            size_type i {0};
-            try {
-                for(; i < old_size; ++i) {
-                    ds::construct(new_buffer + i, ds::move(this->first[i]));
-                }
-            }catch(...) {
-                ds::destroy(this->first, this->first + old_size);
-                ds::destroy(new_buffer, new_buffer + i);
-                allocator.deallocate(this->first, old_size);
-                allocator.deallocate(new_buffer, this->buffer_size());
-                throw;
-            }
+        const auto exception_handler {[](pointer old_buffer, size_type old_size, pointer new_buffer,
+                size_type new_size, size_type i, Allocator &allocator) constexpr noexcept -> void {
+            ds::destroy(old_buffer, old_buffer + old_size);
+            ds::destroy(new_buffer, new_buffer + i);
+            allocator.deallocate(old_buffer, old_size);
+            allocator.deallocate(new_buffer, new_size);
+        }};
+        size_type i {0};
+        auto trans {transaction {exception_handler(this->first, old_size, new_buffer,
+                this->buffer_size(), i, allocator)}};
+        for(; i < old_size; ++i) {
+            ds::construct(new_buffer + i, ds::move(this->first[i]));
         }
+        trans.complete();
     }
     ds::destroy(this->first, this->first + old_size);
     allocator.deallocate(this->first, old_size);
     this->first = new_buffer;
 }
 template <typename T, typename Allocator>
-template <typename InputIterator>
-buffer<T, Allocator>::buffer(InputIterator begin, InputIterator end, const Allocator &alloc, false_type) :
-        buffer_size {64, alloc}, first {this->buffer_size.allocator().allocate(this->buffer_size())} {
-    size_type i {0};
-    auto &buffer_size {this->buffer_size()};
-    auto &allocator {this->buffer_size.allocator()};
-    constexpr auto nothrow_moveable {is_nothrow_constructible_v<T, remove_reference_t<decltype(*begin)> &&>};
-    constexpr auto trivially_copyable {is_trivially_copyable_v<T>};
-    if constexpr(nothrow_moveable or is_nothrow_constructible_v<T, decltype(*begin)>) {
-        for(; begin not_eq end; ++i) {
-            if(i == buffer_size) {
-                buffer_size *= 2;
-                pointer new_first {};
-                try {
-                    if constexpr(trivially_copyable) {
-                        this->first = allocator.reallocate(this->first, buffer_size);
-                    }else {
-                        new_first = allocator.allocate(buffer_size);
-                    }
-                }catch(...) {
-                    ds::destroy(this->first, this->first + i);
-                    allocator.deallocate(this->first, i);
-                    throw;
-                }
-                if constexpr(not trivially_copyable) {
-                    this->move_to(new_first, i);
-                }
-            }
-            ds::construct(this->first + i, ds::move_if<nothrow_moveable>(*begin++));
-        }
+template <bool CopyFromValue>
+constexpr void buffer<T, Allocator>::initialize(const T &value) {
+    const auto size {this->buffer_size()};
+    this->first = this->buffer_size.allocator().allocate(size);
+    auto trans {transaction {exception_handler(*this)}};
+    if constexpr(not CopyFromValue and is_trivially_default_constructible_v<T>) {
+        ds::memory_default_initialization(this->first, sizeof(T) * size);
     }else {
-        for(; begin not_eq end; ++i) {
-            if(i == buffer_size) {
-                buffer_size *= 2;
-                pointer new_first {};
-                try {
-                    new_first = allocator.allocate(buffer_size);
-                }catch(...) {
-                    ds::destroy(this->first, this->first + i);
-                    allocator.deallocate(this->first, i);
-                    throw;
-                }
-                this->move_to(new_first, i);
-            }
-            try {
-                ds::construct(this->first + i, ds::move(*begin++));
-            }catch(...) {
-                ds::destroy(this->first, this->first + i);
-                allocator.deallocate(this->first, buffer_size);
-                throw;
-            }
-        }
-    }
-    if(i < buffer_size) {
-        pointer new_first {};
-        try {
-            if constexpr(trivially_copyable) {
-                this->first = allocator.reallocate(this->first, buffer_size);
+        for(auto &i {trans.get_rollback().i}; i < size; ++i) {
+            if constexpr(CopyFromValue) {
+                ds::construct(this->first + i, value);
             }else {
-                new_first = allocator.allocate(buffer_size);
+                ds::construct(this->first + i);
             }
-        }catch(...) {
-            ds::destroy(this->first, this->first + i);
-            allocator.deallocate(this->first, buffer_size);
-            throw;
         }
-        if constexpr(not trivially_copyable) {
-            this->move_to(new_first, i);
-        }
-        buffer_size = i;
     }
+    trans.complete();
+}
+template <typename T, typename Allocator>
+template <typename InputIterator>
+constexpr void buffer<T, Allocator>::initialize(InputIterator begin, InputIterator end, false_type) {
+    auto &size {this->buffer_size()};
+    auto &allocator {this->buffer_size.allocator()};
+    auto trans {transaction {exception_handler(*this)}};
+    auto &i {trans.get_rollback().i};
+    for(; begin not_eq end; ++i) {
+        if(i == size) {
+            size *= 2;
+            if constexpr(is_trivially_copyable_v<T>) {
+                this->first = allocator.reallocate(this->first, size);
+            }else {
+                this->move_to(allocator.allocate(size), i);
+            }
+        }
+        ds::construct(this->first + i, ds::move(*begin++));
+    }
+    if(i not_eq size) {
+        if constexpr(is_trivially_copyable_v<T>) {
+            this->first = allocator.reallocate(this->first, i);
+        }else {
+            this->move_to(allocator.allocate(i), i);
+        }
+        size = i;
+    }
+    trans.complete();
 }
 template <typename T, typename Allocator>
 template <typename ForwardIterator>
-buffer<T, Allocator>::buffer(ForwardIterator begin, ForwardIterator end, const Allocator &allocator, true_type) :
-        buffer_size {static_cast<size_type>(ds::distance(begin, end)), allocator},
-        first {this->buffer_size() == 0 ? nullptr : this->buffer_size.allocator().allocate(this->buffer_size())} {
-    if(begin == end) {
-        return;
-    }
-    const auto buffer_size {this->buffer_size()};
-    if constexpr(is_pointer_v<ForwardIterator> and is_trivially_copy_assignable_v<T>) {
-        ds::memory_copy(this->first, begin, sizeof(T) * buffer_size);
+constexpr void buffer<T, Allocator>::initialize(ForwardIterator begin, ForwardIterator end, true_type) {
+    const auto size {this->buffer_size()};
+    this->first = this->buffer_size.allocator().allocate(size);
+    auto trans {transaction {exception_handler(*this)}};
+    if constexpr(is_trivially_default_constructible_v<T>) {
+        ds::memory_default_initialization(this->first, sizeof(T) * size);
     }else {
-        if constexpr(is_nothrow_constructible_v<T, decltype(*begin)>) {
-            for(auto i {0}; begin not_eq end; ++i) {
-                ds::construct(this->first + i, *begin++);
-            }
-        }else {
-            size_type i {0};
-            try {
-                for(; begin not_eq end; ++i) {
-                    ds::construct(this->first + i, *begin++);
-                }
-            }catch(...) {
-                ds::destroy(this->first, this->first + i);
-                this->buffer_size.allocator().deallocate(this->first, buffer_size);
-                throw;
-            }
+        for(auto &i {trans.get_rollback().i}; i < size; ++i) {
+            ds::construct(this->first + i, *begin++);
         }
     }
+    trans.complete();
 }
 
 /* public functions */
 template <typename T, typename Allocator>
-constexpr buffer<T, Allocator>::buffer(size_type n, const Allocator &allocator) :
-        buffer_size {n}, first {this->buffer_size.allocator().allocate(this->buffer_size())} {
-    if constexpr(is_trivially_default_constructible_v<T>) {
-        ds::memory_default_initialization(this->first, sizeof(T) * this->buffer_size());
-    }else {
-        if constexpr(is_nothrow_default_constructible_v<T>) {
-            for(auto i {0}; i < n; ++i) {
-                ds::construct(this->first + i);
-            }
-        }else {
-            size_type i {0};
-            try {
-                for(; i < this->buffer_size(); ++i) {
-                    ds::construct(this->first + i);
-                }
-            }catch(...) {
-                ds::destroy(this->first, this->first + i);
-                this->buffer_size.allocator().deallocate(this->first, n);
-                throw;
-            }
-        }
-    }
+constexpr buffer<T, Allocator>::buffer(size_type n, const Allocator &allocator) : buffer_size(n, allocator), first {} {
+    this->initialize<false>({});
 }
 template <typename T, typename Allocator>
 constexpr buffer<T, Allocator>::buffer(size_type n, const T &value, const Allocator &allocator) :
-        buffer_size {n}, first {this->buffer_size.allocator().allocate(this->buffer_size())} {
-    if constexpr(is_nothrow_copy_constructible_v<T>) {
-        for(auto i {0}; i < n; ++i) {
-            ds::construct(this->first + i, value);
-        }
-    }else {
-        size_type i {0};
-        try {
-            for(; i < n; ++i) {
-                ds::construct(this->first + i, value);
-            }
-        }catch(...) {
-            ds::destroy(this->first, this->first + i);
-            this->buffer_size.allocator().deallocate(this->first, n);
-            throw;
-        }
-    }
+        buffer_size(n, allocator), first {} {
+    this->initialize(value);
 }
 template <typename T, typename Allocator>
-template <IsInputIterator InputIterator>
+template <typename buffer<T, Allocator>::size_type DefaultSize, typename InputIterator>
+requires (is_input_iterator_v<InputIterator> and not is_forward_iterator_v<InputIterator>)
 constexpr buffer<T, Allocator>::buffer(InputIterator begin, InputIterator end, const Allocator &allocator) :
-        buffer(begin, end, allocator, is_forward_iterator<InputIterator> {}) {}
+        buffer_size(DefaultSize, allocator), first {} {
+    this->initialize(begin, end, false_type {});
+}
 template <typename T, typename Allocator>
 constexpr buffer<T, Allocator>::buffer(initializer_list<T> init_list, const Allocator &allocator) :
-        buffer(init_list.begin(), init_list.end(), allocator, true_type {}) {}
+        buffer(init_list.begin(), init_list.end(), allocator) {}
+
 template <typename T, typename Allocator>
 constexpr buffer<T, Allocator>::~buffer() noexcept {
-    const auto buffer_size {this->buffer_size()};
-    ds::destroy(this->first, this->first + buffer_size);
-    this->buffer_size.allocator().deallocate(this->first, buffer_size);
+    const auto size {this->buffer_size()};
+    ds::destroy(this->first, this->first + size);
+    this->buffer_size.allocator().deallocate(this->first, size);
 }
 template <typename T, typename Allocator>
 constexpr typename buffer<T, Allocator>::const_pointer buffer<T, Allocator>::begin() const & noexcept {
@@ -378,6 +323,8 @@ template <typename T, typename Allocator>
 constexpr Allocator buffer<T, Allocator>::allocator() const noexcept {
     return this->buffer_size.allocator();
 }
-}
+__DATA_STRUCTURE_END
 
-#endif //DATA_STRUCTURE_BUFFER_HPP
+}       // namespace data_structure
+
+#endif      // DATA_STRUCTURE_BUFFER_HPP
