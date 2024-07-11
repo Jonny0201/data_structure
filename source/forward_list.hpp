0f503496ab9aa2b2ce213b3ecdd53346a417bb1f
Jonny <21113168+Jonny0201@users.noreply.github.com>
2024-07-12 00:28:30 +0800

    remove the noexcept specifier from default constructor for forward_list due to the real_allocator throwing all the time

--- source/forward_list.hpp
+++ source/forward_list.hpp
@@ -58,7 +58,7 @@ private:
     constexpr void deallocate_nodes(node_type, node_type) noexcept;
     constexpr void deallocate_nodes(node_type, size_type) noexcept;
 public:
-    constexpr forward_list() noexcept(is_nothrow_default_constructible_v<real_allocator>) = default;
+    constexpr forward_list() = default;
     explicit constexpr forward_list(const Allocator &)
             noexcept(is_nothrow_constructible_v<real_allocator, const Allocator &>);
     explicit constexpr forward_list(size_type, const Allocator & = {});
